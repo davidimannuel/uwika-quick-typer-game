a85bbadfbd80f9e09f2718bbf0cefa2920b59ac5
David Prastiya Imannuel <davidprastiyaimannuel@NB-DavidPrastiyaImannuel-Tech.local>
2025-12-15 19:28:55 +0700

    remove min required password

--- internal/infrastructure/http/dto/dto.go
+++ internal/infrastructure/http/dto/dto.go
@@ -3,7 +3,7 @@ package dto
 // Auth DTOs
 type RegisterRequest struct {
 	Username string `json:"username" binding:"required"`
-	Password string `json:"password" binding:"required,min=6"`
+	Password string `json:"password" binding:"required"`
 }
 
 type LoginRequest struct {
